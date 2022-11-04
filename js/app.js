0cbeeac0b4153ca29a2e9d6891c07d29340ab0b6
Jarrett Guyer <guyer.jarrett@gmail.com>
2022-11-04 09:23:53 -0600

    added cocktail sort algorythm

--- js/app.js
+++ js/app.js
@@ -2,104 +2,142 @@ const ctx = document.getElementById('myChart').getContext('2d');
 
 //Generate array with random numbers between 0 and 100
 function generateArray() {
-    let array = [];
-    for (let i = 0; i < 20; i++) {
-        array.push(Math.floor(Math.random() * 100));
-    }
-    return array;
+  let array = [];
+  for (let i = 0; i < 20; i++) {
+    array.push(Math.floor(Math.random() * 100));
+  }
+  return array;
 }
-
 
 let array = generateArray();
 let colorsArray = generateColorsArray();
 //Bubble sort
-async function bubbleSort(array) {
-    let sortedArray = [...array];
-    let temp;
-    for (let i = 0; i < sortedArray.length; i++) {
-        for (let j = 0; j < sortedArray.length - 1; j++) {
-            if (sortedArray[j] > sortedArray[j + 1]) {
-                temp = sortedArray[j];
-                sortedArray[j] = sortedArray[j + 1];
-                sortedArray[j + 1] = temp;
-                //console.log(sortedArray);
-                //delay
-                await new Promise(resolve => setTimeout(resolve, 500))
-                    .then(updateChart(sortedArray, 'bubble sort'));
-            }
-        }
+async function bubbleSortExample(array) {
+  let count = 0;
+  let sortedArray = [...array]; // copies the randomized array
+  let temp; // sets a temporary hold of one value for swapping later
+  for (let i = 0; i < sortedArray.length; i++) {
+    for (let j = 0; j < sortedArray.length - 1; j++) {
+      if (sortedArray[j] > sortedArray[j + 1]) {
+        temp = sortedArray[j];
+        sortedArray[j] = sortedArray[j + 1];
+        sortedArray[j + 1] = temp;
+        //console.log(sortedArray);
+        //delay
+        count++;
+        await new Promise((resolve) => setTimeout(resolve, 500)).then(updateChart(sortedArray, `bubble sort, sort count is ${count}`));
+      }
     }
-    return sortedArray;
+  }
+  return sortedArray;
 }
-bubbleSort(array);
+
+async function cocktailSort(array) {
+  let count = 0;
+  let sortedArray = [...array]; // copies the randomized array
+  let start = 0;
+  let end = array.length - 1;
+  let changed = true;
+
+  while (changed) {
+    for (let i = start; i <= end - 1; i++) {
+      if (sortedArray[i] > sortedArray[i + 1]) {
+        [sortedArray[i], sortedArray[i + 1]] = [sortedArray[i + 1], sortedArray[i]];
+        changed = true;
+        count++;
+        await new Promise((resolve) => setTimeout(resolve, 150)).then(updateChart(sortedArray, `quick sort, sort count is ${count}`));
+      }
+    }
+    if (!changed) {
+      break;
+    }
+    changed = false;
+    end--;
+
+    for (let i = end; i >= start + 1; i--) {
+      if (sortedArray[i] < sortedArray[i - 1]) {
+        [sortedArray[i], sortedArray[i - 1]] = [sortedArray[i - 1], sortedArray[i]];
+        changed = true;
+        count++;
+        await new Promise((resolve) => setTimeout(resolve, 150)).then(updateChart(sortedArray, `quick sort, sort count is ${count}`));
+      }
+    }
+    start++;
+  }
+
+  return sortedArray;
+}
+
+// Call the sorting function
+cocktailSort(array);
+
+// Some magic for colors
 var colors = generateColorsArray();
-//updateChart(array, 'bubble sort');
+
 function updateChart(array, chartLabel) {
-    let chartStatus = Chart.getChart("myChart"); // <canvas> id
-    if (chartStatus != undefined) {
-        chartStatus.destroy();
-    }
-    myChart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-            labels: [...array],
-            datasets: [{
-                label: chartLabel,
-                data: [...array],
-                backgroundColor: generateColorsArrayUsingArrayValues(array),
-                borderColor: generateColorsArrayUsingArrayValues(array),
-                borderWidth: 1
-            }]
+  let chartStatus = Chart.getChart('myChart'); // <canvas> id
+  if (chartStatus != undefined) {
+    chartStatus.destroy();
+  }
+  myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: [...array],
+      datasets: [
+        {
+          label: chartLabel,
+          data: [...array],
+          backgroundColor: generateColorsArrayUsingArrayValues(array),
+          borderColor: generateColorsArrayUsingArrayValues(array),
+          borderWidth: 1,
         },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
-                }
-            },
-            animation: {
-                duration:0
-            },
-            responsive: true,
-            maintainAspectRatio: false,
-            
-        }
-    });
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+      animation: {
+        duration: 0,
+      },
+      responsive: true,
+      maintainAspectRatio: false,
+    },
+  });
 }
 
-// generate random colors array
 function generateColorsArray() {
-    let colors = [];
-    for (let i = 0; i < 20; i++) {
-        colors.push(`rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`);
-    }
-    return colors;
-}
-// generate colors using array values
-// generate random colors array
-function generateColorsArrayUsingArrayValues(array) {
-    let colors = [];
-    for (let i = 0; i < array.length; i++) {
-        colors.push(generateColorBasedOnNumberValue(array[i]));
-    }
-    return colors;
+  let colors = [];
+  for (let i = 0; i < 20; i++) {
+    colors.push(`rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`);
+  }
+  return colors;
 }
 
-    
+function generateColorsArrayUsingArrayValues(array) {
+  let colors = [];
+  for (let i = 0; i < array.length; i++) {
+    colors.push(generateColorBasedOnNumberValue(array[i]));
+  }
+  return colors;
+}
+
 function generateColorBasedOnNumberValue(number) {
-    let colorVal = mapNumberToBetween0And255(number);
-    let color = `rgb(${colorVal}, ${colorVal}, ${colorVal})`;
-    return color;
+  let colorVal = mapNumberToBetween0And255(number);
+  let color = `rgb(${colorVal}, ${colorVal}, ${colorVal})`;
+  return color;
 }
 function mapNumberToBetween0And255(number) {
-    let mappedNumber = Math.floor(number * 2.55);
-    return mappedNumber;
+  let mappedNumber = Math.floor(number * 2.55);
+  return mappedNumber;
 }
-//assign colors to array values
+
 function assignColorsToValues(array, colors) {
-    let newArray = [];
-    for (let i = 0; i < array.length; i++) {
-        newArray.push({ value: array[i], color: colors[i] });
-    }
-    return newArray;
+  let newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    newArray.push({ value: array[i], color: colors[i] });
+  }
+  return newArray;
 }
